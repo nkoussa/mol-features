63c9221233df6fbbe961b1251034d4409d24179c
Alex Partin <25892264+adpartin@users.noreply.github.com>
2020-07-14 15:03:51 -0500

    Specify which fea types to compute

--- src/gen_mol_fea.py
+++ src/gen_mol_fea.py
@@ -47,6 +47,7 @@ OUTDIR = Path(filepath, '../out').resolve()
 
 def parse_args(args):
     parser = argparse.ArgumentParser(description='Generate molecular feature sets.')
+
     parser.add_argument('--smiles_path',
                         type=str,
                         default=SMILES_PATH,
@@ -55,23 +56,30 @@ def parse_args(args):
                         type=str,
                         default=OUTDIR,
                         help=f'Output dir (default: {OUTDIR}).')
+    parser.add_argument('--fea_type',
+                        type=str,
+                        default=['descriptors'],
+                        nargs='+',
+                        choices=['descriptors', 'images', 'fps'],
+                        help='Feature type (default: descriptors).')
     parser.add_argument('--par_jobs',
-                        type=int, 
+                        type=int,
                         default=1,
-                        help=f'Number of joblib parallel jobs (default: 1).')
+                        help='Number of joblib parallel jobs (default: 1).')
     parser.add_argument('--i1',
-                        type=int, 
+                        type=int,
                         default=0,
-                        help=f'Start index of a smiles sample (default: 0).')
+                        help='Start index of a smiles sample (default: 0).')
     parser.add_argument('--i2',
-                        type=int, 
+                        type=int,
                         default=None,
-                        help=f'End index of smiles sample (default: None).')
+                        help='End index of smiles sample (default: None).')
+
     args, other_args = parser.parse_known_args(args)
     return args
 
 
-def add_fea_prfx(df, prfx:str, id0:int):
+def add_fea_prfx(df, prfx: str, id0: int):
     """ Add prefix feature columns. """
     return df.rename(columns={s: prfx+str(s) for s in df.columns[id0:]})
 
@@ -92,20 +100,28 @@ def get_image(mol):
 
 
 def run(args):
-    import ipdb; ipdb.set_trace()
+    import ipdb; ipdb.set_trace(context=5)
     t0 = time()
     smiles_path = args['smiles_path']
     par_jobs = args['par_jobs']
-    
+    fea_type = args['fea_type']
+
     print('\nLoad SMILES ...')
     smiles_path = Path(args['smiles_path'])
+
+    # Load covid-19
     # smi = pd.read_csv( smiles_path, sep='\t', names=['SMILES', 'TITLE'] )
-    smi = pd.read_csv( smiles_path )
+    # smi = pd.read_csv(smiles_path)
+
+    # Load nci60
+    smi = pd.read_csv(smiles_path, sep='\t')
+    smi = smi.rename(columns={'NSC.ID': 'TITLE'})
+
     smi = smi.astype({'SMILES': str, 'TITLE': str})
     smi['SMILES'] = smi['SMILES'].map(lambda x: x.strip())
     smi['TITLE'] = smi['TITLE'].map(lambda x: x.strip())
     n_smiles = smi.shape[0]
-    fea_id0 = smi.shape[1] # this used as index where features begin
+    fea_id0 = smi.shape[1]  # this used as index where features begin
 
     # Create outdir
     i1, i2 = args['i1'], args['i2']
@@ -113,17 +129,17 @@ def run(args):
     if i2 is None:
         i2 = n_smiles
     # outdir = Path(args['outdir'])/date/ids_dir
-    outdir = Path(args['outdir'])/ids_dir
-    os.makedirs( outdir, exist_ok=True )
+    outdir = Path(args['outdir'], ids_dir)
+    os.makedirs(outdir, exist_ok=True)
 
     # Logger
-    lg = Logger( outdir/'gen.fea.dfs.log' )
+    lg = Logger(outdir/'gen.fea.dfs.log')
     print_fn = get_print_func( lg.logger )
-    print_fn( f'File path: {filepath}' )
-    print_fn( f'\n{pformat(args)}' )
+    print_fn(f'File path: {filepath}')
+    print_fn(f'\n{pformat(args)}')
 
-    print_fn('\nInput data path  {}'.format( smiles_path ))
-    print_fn('Output data dir  {}'.format( outdir ))
+    print_fn('\nInput data path  {}'.format(smiles_path))
+    print_fn('Output data dir  {}'.format(outdir))
 
     # Duplicates
     # dup = smi[ smi.duplicated(subset=['smiles'], keep=False) ].reset_index(drop=True)
@@ -131,107 +147,106 @@ def run(args):
 
     # Drop duplicates
     smi = drop_dup_rows(smi, print_fn)
-    
+
     # Exract subset SMILES
     smi = smi.iloc[i1:i2+1, :].reset_index(drop=True)
 
     print_fn('\nCanonicalize SMILES ...')
-    can_smi_vec = canon_smiles( smi['SMILES'], par_jobs=par_jobs )
+    can_smi_vec = canon_smiles(smi['SMILES'], par_jobs=par_jobs)
     can_smi_vec = pd.Series(can_smi_vec)
 
     # Save bad SMILES to file (that were not canonicalized)
     nan_ids = can_smi_vec.isna()
-    bad_smi = smi[ nan_ids ]
+    bad_smi = smi[nan_ids]
     if len(bad_smi) > 0:
         bad_smi.to_csv(outdir/'smi_canon_err.csv', index=False)
 
     # Keep the good (canonicalized) SMILES
     smi['SMILES'] = can_smi_vec
-    smi = smi[ ~nan_ids ].reset_index(drop=True)
-
+    smi = smi[~nan_ids].reset_index(drop=True)
 
     # ========================================================
     # Generate imgaes
-    # --------------------------
-    images = smiles_to_images(smi, smi_col_name='SMILES', title_col_name='TITLE',
-            molSize=(128, 128), kekulize=True, par_jobs=par_jobs)
-    # print(images[0].keys())
-    img_outpath = outdir/f'images.ids.{i1}-{i2}.pkl'
+    # ---------------
+    if 'images' in fea_type:
+        images = smiles_to_images(smi, smi_col_name='SMILES', title_col_name='TITLE',
+                                  molSize=(128, 128), kekulize=True, par_jobs=par_jobs)
+        # print(images[0].keys())
+        img_outpath = outdir/f'images.ids.{i1}-{i2}.pkl'
 
-    # Dump images to file (list of dicts)
-    pickle.dump( images, open(img_outpath, 'wb') )
-    # Load pkl
-    # aa = pickle.load(open(img_outpath, 'rb'))
-    # sum(images[0]['img'].reshape(-1,)-aa[0]['img'].reshape(-1,))
-
+        # Dump images to file (list of dicts)
+        pickle.dump(images, open(img_outpath, 'wb'))
+        # Load pkl
+        # aa = pickle.load(open(img_outpath, 'rb'))
+        # sum(images[0]['img'].reshape(-1,)-aa[0]['img'].reshape(-1,))
 
     # ========================================================
     # Generate fingerprints
-    # --------------------------
-    def gen_fps_and_save(smi, radius=1, par_jobs=par_jobs):
-        file_format = 'parquet'
-        ecfp = smiles_to_fps(smi, smi_name='SMILES', radius=radius, par_jobs=par_jobs)
-        ecfp = add_fea_prfx(ecfp, prfx=f'ecfp{2*radius}.', id0=fea_id0)
-        ecfp.to_parquet( outdir/f'ecfp{2*radius}.ids.{i1}-{i2}.{file_format}' )
-        del ecfp
+    # ---------------------
+    if 'fps' in fea_type:
+        def gen_fps_and_save(smi, radius=1, par_jobs=par_jobs):
+            file_format = 'parquet'
+            ecfp = smiles_to_fps(smi, smi_name='SMILES', radius=radius, par_jobs=par_jobs)
+            ecfp = add_fea_prfx(ecfp, prfx=f'ecfp{2*radius}.', id0=fea_id0)
+            ecfp.to_parquet( outdir/f'ecfp{2*radius}.ids.{i1}-{i2}.{file_format}' )
+            del ecfp
 
-    gen_fps_and_save(smi, radius=1, par_jobs=par_jobs)
-    gen_fps_and_save(smi, radius=2, par_jobs=par_jobs)
-    gen_fps_and_save(smi, radius=3, par_jobs=par_jobs)
-
+        gen_fps_and_save(smi, radius=1, par_jobs=par_jobs)
+        gen_fps_and_save(smi, radius=2, par_jobs=par_jobs)
+        gen_fps_and_save(smi, radius=3, par_jobs=par_jobs)
 
     # ========================================================
     # Generate descriptors
-    # --------------------------
-    dd = smiles_to_mordred(smi, smi_name='SMILES', par_jobs=par_jobs)
-    dd = add_fea_prfx(dd, prfx='dd_', id0=fea_id0)
+    # --------------------
+    if 'descriptors' in fea_type:
+        dd = smiles_to_mordred(smi, smi_name='SMILES', par_jobs=par_jobs)
+        dd = add_fea_prfx(dd, prfx='dd_', id0=fea_id0)
 
-    # Filter NaNs (step 1)
-    # Drop rows where all values are NaNs
-    print_fn('\nDrop rows where all values are NaN ...')
-    print_fn('Shape: {}'.format( dd.shape ))
-    idx = ( dd.isna().sum(axis=1) == dd.shape[1] ).values
-    dd = dd.iloc[~idx, :].reset_index(drop=True)
-    # Drop cols where all values are NaNs
-    # idx = ( dd.isna().sum(axis=0) == dd.shape[0] ).values
-    # dd = dd.iloc[:, ~idx].reset_index(drop=True)
-    print_fn('Shape: {}'.format( dd.shape ))
+        # Filter NaNs (step 1)
+        # Drop rows where all values are NaNs
+        print_fn('\nDrop rows where all values are NaN ...')
+        print_fn('Shape: {}'.format(dd.shape))
+        idx = ( dd.isna().sum(axis=1) == dd.shape[1] ).values
+        dd = dd.iloc[~idx, :].reset_index(drop=True)
+        # Drop cols where all values are NaNs
+        # idx = ( dd.isna().sum(axis=0) == dd.shape[0] ).values
+        # dd = dd.iloc[:, ~idx].reset_index(drop=True)
+        print_fn('Shape: {}'.format(dd.shape))
 
-    # Filter NaNs (step 2)
-    # Drop rows and cols based on a thershold of NaN values
-    # print(dd.isna().sum(axis=1).sort_values(ascending=False))
-    # p=dd.isna().sum(axis=1).sort_values(ascending=False).hist(bins=100);
-    th = 0.2
-    print_fn('\nDrop rows with at least {} NaNs (at least {} out of {}).'.format(
-        th, int(th * dd.shape[1]), dd.shape[1]))
-    print_fn('Shape: {}'.format( dd.shape ))
-    dd = dropna(dd, axis=1, th=th)
-    print_fn('Shape: {}'.format( dd.shape ))
+        # Filter NaNs (step 2)
+        # Drop rows and cols based on a thershold of NaN values
+        # print(dd.isna().sum(axis=1).sort_values(ascending=False))
+        # p=dd.isna().sum(axis=1).sort_values(ascending=False).hist(bins=100);
+        th = 0.2
+        print_fn('\nDrop rows with at least {} NaNs (at least {} out of {}).'.format(
+            th, int(th * dd.shape[1]), dd.shape[1]))
+        print_fn('Shape: {}'.format(dd.shape))
+        dd = dropna(dd, axis=1, th=th)
+        print_fn('Shape: {}'.format(dd.shape))
 
-    # Cast features (descriptors)
-    print_fn('\nCast descriptors to float ...')
-    dd = dd.astype({c: np.float32 for c in dd.columns[fea_id0:]})
+        # Cast features (descriptors)
+        print_fn('\nCast descriptors to float ...')
+        dd = dd.astype({c: np.float32 for c in dd.columns[fea_id0:]})
 
-    # Impute missing values
-    print_fn('\nImpute NaNs ...')
-    print_fn('Total NaNs: {}'.format( dd.isna().values.flatten().sum() ))
-    dd = dd.fillna(0.0)
-    print_fn('Total NaNs: {}'.format( dd.isna().values.flatten().sum() ))
+        # Impute missing values
+        print_fn('\nImpute NaNs ...')
+        print_fn('Total NaNs: {}'.format( dd.isna().values.flatten().sum() ))
+        dd = dd.fillna(0.0)
+        print_fn('Total NaNs: {}'.format( dd.isna().values.flatten().sum() ))
 
-    # Save
-    print_fn('\nSave ...')
-    dd = dd.reset_index(drop=True)
-    file_format = 'parquet'
-    dd.to_parquet( outdir/'dd.ids.{}-{}.{}'.format(i1, i2, file_format) )
-    # dd.to_csv( outdir/'dd.ids.{}-{}.{}'.format(i1, i2, file_format), index=False )
-
+        # Save
+        print_fn('\nSave ...')
+        dd = dd.reset_index(drop=True)
+        file_format = 'parquet'
+        dd.to_parquet( outdir/'dd.ids.{}-{}.{}'.format(i1, i2, file_format) )
+        # dd.to_csv( outdir/'dd.ids.{}-{}.{}'.format(i1, i2, file_format), index=False )
 
     # ========================================================
     print_fn('\nRuntime {:.2f} mins'.format( (time()-t0)/60 ))
     print_fn('Done.')
     lg.kill_logger()
-    
-    
+
+
 def main(args):
     args = parse_args(args)
     args = vars(args)
@@ -240,5 +255,3 @@ def main(args):
 
 if __name__ == '__main__':
     main(sys.argv[1:])
-
-
